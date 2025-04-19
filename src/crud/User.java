c7fd8c4abae6c1e994920e71d5aa7534451691fb
Agus Seputra <agusseputra@Aguss-MacBook-Pro.local>
2025-04-19 13:41:31 +0800

    first commit

---
+++ src/crud/User.java
@@ -0,0 +1,69 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package crud;
+
+/**
+ *
+ * @author agusseputra
+ */
+
+public class User {
+    private int id;
+    private String lastName;
+    private String email;
+    private String country;
+    
+     public User(int id, String firstName,String lastName, String email, String country){
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.country = country;
+    }
+     
+     
+    private String firstName;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+    
+}
